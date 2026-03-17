4ed5d6f322ce4e21bfe9a2cc286d7acb54237a68
votre nom complet <nobel71@ulaval.ca>
2026-03-17 13:50:02 -0400

    Fix collision formula, draw dimensions, cactus spawning, and timer shadowing

--- src/DinosaurGame.java
+++ src/DinosaurGame.java
@@ -1,133 +1,92 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.event.*;
 import java.util.ArrayList;
 
-public class DinosaurGame extends JPanel implements ActionListener, KeyListener{
+public class DinosaurGame extends JPanel implements ActionListener, KeyListener {
 
-    int boardWidth = 750;
-    int boardHeight = 250;
-    int score =0;
+    static final int BOARD_WIDTH = 750;
+    static final int BOARD_HEIGHT = 250;
+    static final int GRAVITY = 2;
+    static final int DINO_WIDTH = 88;
+    static final int DINO_HEIGHT = 94;
+    static final int DINO_X = 50;
+    static final int CACTUS1_W = 34, CACTUS2_W = 69, CACTUS3_W = 103;
+    static final int CACTUS_H = 70;
 
-    int gravity = 2;
-
-    private Image cactus1;
-    private Image cactus2;
-    private Image cactus3;
-
-    private Image dinosaurImage;
+    private Image dinosaurRunImage;
     private Image dinosaurDeadImage;
     private Image dinosaurJumpImage;
+    private Image cactus1Img;
+    private Image cactus2Img;
+    private Image cactus3Img;
 
-    int dinosaurWidth = 88;
-    int dinosaurHeight = 94;
-    int dinosaurX = 50;
-    int dinosaurY = boardHeight - dinosaurHeight;
-
+    int dinosaurGroundY;
     Block dinosaur;
 
-    int cactus1Width = 34;
-    int cactus2Width = 69;
-    int cactus3Width = 103;
-
-    int cactusHeight = 70;
-    int cactusX = 700;
-    int cactusY = boardHeight-cactusHeight;
-
-    ArrayList<Block> cactusArray= new ArrayList<>();
-
+    ArrayList<Block> obstacles = new ArrayList<>();
 
     Timer gameLoop;
-    Timer placeCactusTimer;
+    Timer placeObstacleTimer;
 
-    int velocityY =0;
-
-    int cactusVelocity=20;
-
+    int velocityY = 0;
+    int speed = 8;
+    int score = 0;
+    int highScore = 0;
     boolean gameOver = false;
 
-
-
-
-
-
-    public DinosaurGame(){
-
-        setPreferredSize(new Dimension(this.boardWidth,this.boardHeight));
-        setBackground(Color.GRAY);
+    public DinosaurGame() {
+        setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
+        setBackground(Color.WHITE);
         setFocusable(true);
         addKeyListener(this);
 
+        dinosaurRunImage = new ImageIcon(getClass().getResource("./images/dino-run.gif")).getImage();
         dinosaurDeadImage = new ImageIcon(getClass().getResource("./images/dino-dead.png")).getImage();
-        dinosaurImage = new ImageIcon(getClass().getResource("./images/dino-run.gif")).getImage();
         dinosaurJumpImage = new ImageIcon(getClass().getResource("./images/dino-jump.png")).getImage();
+        cactus1Img = new ImageIcon(getClass().getResource("./images/cactus1.png")).getImage();
+        cactus2Img = new ImageIcon(getClass().getResource("./images/cactus2.png")).getImage();
+        cactus3Img = new ImageIcon(getClass().getResource("./images/cactus3.png")).getImage();
 
-        cactus1 = new ImageIcon(getClass().getResource("./images/cactus1.png")).getImage();
-        cactus2 = new ImageIcon(getClass().getResource("./images/cactus2.png")).getImage();
-        cactus3 = new ImageIcon(getClass().getResource("./images/cactus3.png")).getImage();
+        dinosaurGroundY = BOARD_HEIGHT - DINO_HEIGHT;
+        dinosaur = new Block(dinosaurRunImage, DINO_X, dinosaurGroundY, DINO_WIDTH, DINO_HEIGHT);
 
-        dinosaur = new Block(dinosaurImage,dinosaurX,dinosaurY, dinosaurHeight,dinosaurWidth);
-        gameLoop = new Timer(1000/60, this);
+        gameLoop = new Timer(1000 / 60, this);
+        placeObstacleTimer = new Timer(1500, e -> placeObstacle());
         gameLoop.start();
-
-
-        Timer placeCactusTimer = new Timer(1500, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                move();
-                placeCactus();
-                repaint();
-            }
-        });
-        placeCactusTimer.start();
-
-
-    }
-    public void placeCactus(){
-        if(gameOver){
-            return;
-        }
-
-        double placeCactusChance = Math.random();
-
-        if(placeCactusChance <0.90){
-            Block cactusAffiche = new Block(cactus3,cactusX,cactusY,cactusHeight,cactus3Width);
-            cactusArray.add(cactusAffiche);
-        }
-        if(placeCactusChance >0.60){
-            Block cactusAffiche = new Block(cactus2,cactusX,cactusY,cactusHeight,cactus2Width);
-            cactusArray.add(cactusAffiche);
-        }
-        else{
-            Block cactusAffiche = new Block(cactus1,cactusX,cactusY,cactusHeight,cactus1Width);
-            cactusArray.add(cactusAffiche);
-        }
-
-
+        placeObstacleTimer.start();
     }
 
+    void placeObstacle() {
+        if (gameOver) return;
+        double r = Math.random();
+        if (r < 0.33) {
+            obstacles.add(new Block(cactus1Img, BOARD_WIDTH, BOARD_HEIGHT - CACTUS_H, CACTUS1_W, CACTUS_H));
+        } else if (r < 0.66) {
+            obstacles.add(new Block(cactus2Img, BOARD_WIDTH, BOARD_HEIGHT - CACTUS_H, CACTUS2_W, CACTUS_H));
+        } else {
+            obstacles.add(new Block(cactus3Img, BOARD_WIDTH, BOARD_HEIGHT - CACTUS_H, CACTUS3_W, CACTUS_H));
+        }
+    }
 
-    class Block {
-
+    static class Block {
         Image image;
-        int x;
-        int y;
+        int x, y, width, height;
 
-        int blockWidth;
-        int blockHeight;
-
-
-        public Block(Image image, int x, int y, int blockHeight, int blockWidth){
-
-            this.x=x;
-            this.y=y;
-
-            this.blockHeight = blockHeight;
-            this.blockWidth = blockWidth;
-
+        Block(Image image, int x, int y, int width, int height) {
             this.image = image;
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
         }
+    }
+
+    boolean collision(Block a, Block b) {
+        return a.x < b.x + b.width  - 5 &&
+               b.x < a.x + a.width  - 5 &&
+               a.y < b.y + b.height - 5 &&
+               b.y < a.y + a.height - 5;
     }
 
     @Override
@@ -135,88 +94,68 @@ public class DinosaurGame extends JPanel implements ActionListener, KeyListener{
         move();
         repaint();
         if (gameOver) {
-            placeCactusTimer.stop();
             gameLoop.stop();
+            placeObstacleTimer.stop();
         }
-
     }
-    @Override
-    public void keyTyped(KeyEvent e) {}
+
+    @Override public void keyTyped(KeyEvent e) {}
+    @Override public void keyReleased(KeyEvent e) {}
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if(gameOver){
-            return;
+        if (gameOver) return;
+        if (e.getKeyCode() == KeyEvent.VK_UP && dinosaur.y == dinosaurGroundY) {
+            velocityY = -17;
+            dinosaur.image = dinosaurJumpImage;
         }
-        if(e.getKeyCode()==KeyEvent.VK_UP){
-                velocityY=(dinosaur.y==dinosaurY)?-17:0;
-                dinosaur.image = (dinosaur.y<=dinosaurY)?dinosaurJumpImage:dinosaurImage;
-            }
-        if(e.getKeyCode()==KeyEvent.VK_DOWN){}
-        if(e.getKeyCode()==KeyEvent.VK_RIGHT){}
-        if(e.getKeyCode()==KeyEvent.VK_LEFT){}
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {}
-
-
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         draw(g);
     }
 
-    public void draw(Graphics g){
-
-        g.drawImage(dinosaur.image,dinosaur.x,dinosaur.y,dinosaur.blockHeight, dinosaur.blockHeight,null);
-
-        for(Block cactus:cactusArray) {
-            g.drawImage(cactus.image,cactus.x,cactus.y,cactus.blockHeight, cactus.blockHeight,null);
-
+    void draw(Graphics g) {
+        g.drawImage(dinosaur.image, dinosaur.x, dinosaur.y, dinosaur.width, dinosaur.height, null);
+        for (Block obs : obstacles) {
+            g.drawImage(obs.image, obs.x, obs.y, obs.width, obs.height, null);
         }
-        g.setColor(Color.black);
+        g.setColor(Color.BLACK);
         g.setFont(new Font("Courier", Font.PLAIN, 32));
         if (gameOver) {
-            g.drawString("Game Over: " + String.valueOf(score), 10, 35);
-        }
-        else {
+            g.drawString("Game Over: " + score, 10, 35);
+        } else {
             g.drawString(String.valueOf(score), 10, 35);
         }
-
-
-    }
-    public boolean collision(Block block1, Block block2 ){
-        return block1.x<block2.x+ block2.blockWidth &&
-                block2.x<block1.x+ block1.blockWidth &&
-                block1.y<block2.y+ block2.blockHeight -20 &&
-                block2.x<block1.x+ block1.blockHeight -20;
     }
 
+    void move() {
+        if (gameOver) return;
 
-    public void move() {
-        if(gameOver){
-            return;
-        }
         dinosaur.y += velocityY;
-            if (dinosaur.y < dinosaurY) {
-                velocityY += gravity;
-            } else {
-                dinosaur.y = dinosaurY;
-                dinosaur.image = dinosaurImage;
-            }
-            for (Block cactus : cactusArray) {
-                if(!collision(cactus,dinosaur)) {
-                    cactus.x -= cactusVelocity;
-                    if(dinosaur.x>cactus.x+cactus.blockWidth){
-                    }
-                }
-                else{
-                    dinosaur.image=dinosaurDeadImage;
-                    gameOver = true;
-                }
-
-            }
-        score++;
-
+        if (dinosaur.y < dinosaurGroundY) {
+            velocityY += GRAVITY;
+        } else {
+            dinosaur.y = dinosaurGroundY;
+            velocityY = 0;
+            dinosaur.image = dinosaurRunImage;
         }
+
+        ArrayList<Block> toRemove = new ArrayList<>();
+        for (Block obs : obstacles) {
+            obs.x -= speed;
+            if (collision(obs, dinosaur)) {
+                dinosaur.image = dinosaurDeadImage;
+                gameOver = true;
+                if (score > highScore) highScore = score;
+                return;
+            }
+            if (obs.x + obs.width < 0) toRemove.add(obs);
+        }
+        obstacles.removeAll(toRemove);
+
+        score++;
     }
+}
